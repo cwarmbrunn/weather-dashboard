b9f55c11dec16b03110af53849542dd4705d80f8
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-03-10 09:37:29 -0500

    set up script.js and added tweaks to stylesheet and HTML

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+/* Declare User Form Variable */
+var userFormEl = document.getElementById("#user-form");
+
+/* Declare City Input Variable */
+var cityInputEl = document.querySelector("#city");
+
+/* Declare City Container Variable */
+var cityContainerEl = document.querySelector("#weather-container");
+/* Declare City Search Term Variable */ 
+var citySearchTerm = document.querySelector("#city-search-term");
+
+/* Declare City Buttons Variable */ 
+var cityButtonsEl = document.querySelector("#city-buttons");
