b35af29155e3fa27fd290de6b39100c10f15f87c
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-03-11 10:29:49 -0500

    added response.ok and else statements to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,60 @@
 /* API Key */
 const apiKey = "449a366ead9b85f401e6712d2454211d";
 
-var testKey =
-  "http://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&appid=" +
-  apiKey;
-
-console.log(testKey);
-
-// Declare City to Store Searched Variable */
+/* CITY DECLARATION - Declare City to Store Searched Variable */
 var city = "";
 
-/* Declare User Form Variable */
+/* PAST CITY DECLARATION - Declare pastCity to Store Past Searched Variable */
+var pastCity = "";
+
+/* USER FORM - Declare User Form Variable */
 var userFormEl = document.getElementById("user-form");
 
-/* Declare City Input Variable */
-var cityInputEl = document.querySelector("#form-input");
+/*CITY INPUT - Declare City Input Variable */
+var cityInputEl = document.querySelector(".form-input");
 
-/* Declare City Search Term Variable */
+/* CITY SEARCH TERM - Declare City Search Term Variable */
 var citySearchTerm = document.querySelector("#city-search-term");
+
+/* CITY SEARCH HISTORY - Declare Container for City Search History */
+var citySearchContainer = document.getElementById("city-history");
+
+/* CURRENT WEATHER ICON - Declare Current Weather Icon */
+var cityWeatherIcon = document.getElementById("current-icon");
 
 // TODO: Find a way to use localStorage to store persistent data (past city searches)
 var formSubmitHandler = function (event) {
   // Stops page from refreshing
   event.preventDefault();
 
+  var city = cityInputEl.value.trim();
   // Gets value from the city input element and runs getCityWeather function
-  var city = cityInputEl;
-  console.log("Hello World!");
   if (city) {
-    getCityWeather(city).trim();
+    getCityWeather(city);
     // Clear out old content
-    cityContainerEl.textContent = "";
     cityInputEl.value = "";
   }
-
-  /* TODO: Fix else statement to check for valid city name 
-else {
- alert("Please enter a valid U.S. city name");
- }
- */
 };
 
 var getCityWeather = function (city) {
-  // Format the One Call API - Weather Dashboard UR
-
   // current.uvi  = Current UV index
-  fetch(
-    "http://api.openweathermap.org/data/2.5/onecall?" +
-      { city } +
-      "&appid=" +
-      { apiKey }
-  );
+  var apiUrl =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    city +
+    "&appid=" +
+    apiKey;
+
+  fetch(apiUrl).then(function (response) {
+    console.log(response);
+
+    if (response.ok) {
+      response.json().then(function (data) {});
+
+      // Else statement if city does not return with ok response
+    } else {
+      alert("Error - City " + response.statusText);
+    }
+  });
 };
 
 // Add Event Listeners to Forms
